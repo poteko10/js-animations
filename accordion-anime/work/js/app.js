dbca391a2da721fd8f9c8899f486c2992b6a34b7
poteko <h9007ione@gmail.com>
2025-05-25 19:22:08 +0900

    修正

--- accordion-anime/work/js/app.js
+++ accordion-anime/work/js/app.js
@@ -4,6 +4,11 @@ accordions.forEach(accordion => {
     accordion.addEventListener('click', () => {
       const content = accordion.querySelector(`.list-content`);
       content.classList.toggle(`active`);
+        if (content.style.height === '0px' || content.style.height === '') {
+            content.style.height = content.scrollHeight + 'px'; 
+        } else {
+            content.style.height = '0px'; 
+        }
     });
   });
   
